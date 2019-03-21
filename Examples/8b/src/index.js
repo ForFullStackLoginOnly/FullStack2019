5ee9bedc17ddaa15e23715a386dcbe27d2613f59
NorthernLion <joppetaja@gmail.com>
2019-03-21 12:01:28 +0200

    Example part 2

--- Examples/8b/src/index.js
+++ Examples/8b/src/index.js
@@ -2,33 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+import App from './App'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
 })
 
-const query = gql`
-{
-  allPersons  {
-    name,
-    phone,
-    address {
-      street, city
-    }
-    id
-  }
-}
-`
-
-client.query({ query })
-  .then((response) => {
-    console.log(response.data)
-  })
-
-const App = () => {
-  return <div>
-    test
-  </div>
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ApolloProvider client={client} >
+    <ApolloHooksProvider>
+      <App />
+    </ApolloHooksProvider>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
